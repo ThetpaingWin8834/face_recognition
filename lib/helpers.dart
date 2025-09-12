b457e23fb92075da5d1b4d47f4252bd0d877ac5b
Thet Paing Win <www.thetpaingwin.tpw@gmail.com>
2025-09-12 09:46:06 +0630

    Create helpers.dart

---
+++ lib/helpers.dart
@@ -0,0 +1,12 @@
+import 'dart:developer';
+
+void printLog(dynamic d, {StackTrace? s, String? tag}) {
+  final stacktrace = StackTrace.current;
+  final stackLines = stacktrace.toString().split('\n');
+  final callerInfo = stackLines.length > 1 ? stackLines[1] : 'Unknown';
+  final mtag = callerInfo.split('(').first.trim();
+  log(
+    '${'$mtag ${tag ?? ''}'} : $d, \n ${s != null ? 'stackTrace $s' : ''} '
+        .toString(),
+  );
+}
