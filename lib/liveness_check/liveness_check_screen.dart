81af05d634c2dd04a6fa59664e94e4a43c879822
Thet Paing Win <www.thetpaingwin.tpw@gmail.com>
2026-01-17 15:11:38 +0630

    Create liveness_check_screen.dart

---
+++ lib/liveness_check/liveness_check_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LivenessCheckScreen extends StatefulWidget {
+  const LivenessCheckScreen({super.key});
+
+  @override
+  State<LivenessCheckScreen> createState() => _LivenessCheckScreenState();
+}
+
+class _LivenessCheckScreenState extends State<LivenessCheckScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
