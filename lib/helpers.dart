b52f603039b738626558305f1de7db562dac8909
Thet Paing Win <www.thetpaingwin.tpw@gmail.com>
2026-01-18 20:05:34 +0630

    Update helpers.dart

--- lib/helpers.dart
+++ lib/helpers.dart
@@ -1,5 +1,13 @@
 import 'dart:developer';
 
+String get currentMethod {
+   final stacktrace = StackTrace.current;
+  final stackLines = stacktrace.toString().split('\n');
+  final callerInfo = stackLines.length > 1 ? stackLines[1] : 'Unknown';
+  final mtag = callerInfo.split('(').first.trim();
+  return mtag;
+}
+
 void printLog(dynamic d, {StackTrace? s, String? tag}) {
   final stacktrace = StackTrace.current;
   final stackLines = stacktrace.toString().split('\n');
