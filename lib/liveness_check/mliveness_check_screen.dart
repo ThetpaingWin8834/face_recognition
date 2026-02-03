3278fad9e37fee45115d0496ce1a1b757687c7d8
Thet Paing Win <www.thetpaingwin.tpw@gmail.com>
2026-02-03 18:26:54 +0630

    Create mliveness_check_screen.dart

---
+++ lib/liveness_check/mliveness_check_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MlivenessCheckScreen extends StatelessWidget {
+  const MlivenessCheckScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
