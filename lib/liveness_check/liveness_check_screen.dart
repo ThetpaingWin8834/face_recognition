9f84b6a599cb39bfc30a995b2d791cc8bb27b233
Thet Paing Win <www.thetpaingwin.tpw@gmail.com>
2026-02-04 10:09:48 +0630

    Update liveness_check_screen.dart

--- lib/liveness_check/liveness_check_screen.dart
+++ lib/liveness_check/liveness_check_screen.dart
@@ -41,6 +41,8 @@ class _LivenessCheckScreenState extends State<LivenessCheckScreen>
   late final livenessChecker = LivenessChecker(features: widget.features);
   bool _loading = true;
   LivenessCheckError? _error;
+  int _maxRetryCount = 3;
+
   @override
   void initState() {
     super.initState();
@@ -60,8 +62,11 @@ class _LivenessCheckScreenState extends State<LivenessCheckScreen>
     _cameraController = null;
   }
 
-  void _initCameraController() async {
+  Future<void> _initCameraController() async {
     try {
+      await _cameraController?.pausePreview();
+      await _cameraController?.dispose();
+      _cameraController = null;
       setState(() {
         _loading = true;
         _error = null;
@@ -85,29 +90,33 @@ class _LivenessCheckScreenState extends State<LivenessCheckScreen>
 
       setState(() {});
     } catch (e) {
-      setState(() {
-        _error = InitializedError(
-          message: 'Failed to initialize camera!',
-          rawError: e,
-        );
-      });
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text(e.toString()),
-          duration: Duration(days: 1),
-          action: SnackBarAction(
-            label: 'Close',
-            onPressed: () {
-              SystemNavigator.pop();
-            },
-          ),
-        ),
+      _onError(
+        InitializedError(message: 'Failed to initialize camera!', rawError: e),
       );
     } finally {
       setState(() {
         _loading = false;
       });
     }
+  }
+
+  void _onError(LivenessCheckError error) async {
+    if (error is InitializedError || _maxRetryCount > 0) {
+      _maxRetryCount = --_maxRetryCount;
+        printLog(error.toString());
+
+      await _initCameraController();
+    } else {
+      setState(() {
+        _error = error;
+      });
+    }
+    // setState(() {
+    //   _error = InitializedError(
+    //     message: 'Failed to initialize camera!',
+    //     rawError: e,
+    //   );
+    // });
   }
 
   @override
@@ -144,14 +153,32 @@ class _LivenessCheckScreenState extends State<LivenessCheckScreen>
     // }
   }
 
-  void _onLifeCycleResumed() {
-    final controller = _cameraController;
-    if (controller == null || !controller.value.isInitialized) {
-      _initCameraController();
-      return;
+  void _onLifeCycleResumed() async {
+    try {
+      final controller = _cameraController;
+      if (controller == null || !controller.value.isInitialized) {
+        printLog('_onLifeCycleResumed');
+        await _initCameraController();
+        return;
+      }
+      setState(() {
+        _loading = true;
+      });
+      await _cameraController!.resumePreview();
+      if(!controller.value.isStreamingImages){
+       await controller.startImageStream((image) {});
+
+      }
+    } catch (e) {
+      printLog(e);
+      _onError(
+        InitializedError(message: 'Failed to initialize camera when resume!', rawError: e),
+      );
+    } finally {
+      setState(() {
+        _loading = false;
+      });
     }
-    _cameraController!.resumePreview();
-    controller.startImageStream((image) {});
   }
 
   void _onLifeCycleNotResumed() {
@@ -165,67 +192,69 @@ class _LivenessCheckScreenState extends State<LivenessCheckScreen>
 
   @override
   Widget build(BuildContext context) {
-    return CameraPermissionChecker(
-      child: Builder(
-        builder: (context) {
-          if (_error != null) {
-            return widget.errorBuilder?.call(_error!) ??
-                _DefaultErrorView(error: _error!);
-          } else if (_loading) {
-            return widget.loadingBuilder?.call() ?? _DefaultLoadingView();
-          } else if (_cameraController != null) {
-            return Stack(
-              children: [
-                widget.previewBuilder?.call(_cameraController!) ??
-                    CameraPreview(_cameraController!),
-              ],
-            );
-          }
-          return SizedBox.shrink();
-        },
-      ),
+    return Scaffold(
+      body: CameraPermissionChecker(
+        child: Builder(
+          builder: (context) {
+            if (_error != null) {
+              return widget.errorBuilder?.call(_error!) ??
+                  _DefaultErrorView(error: _error!);
+            } else if (_loading) {
+              return widget.loadingBuilder?.call() ?? _DefaultLoadingView();
+            } else if (_cameraController != null) {
+              return Stack(
+                children: [
+                  widget.previewBuilder?.call(_cameraController!) ??
+                      CameraPreview(_cameraController!),
+                ],
+              );
+            }
+            return SizedBox.shrink();
+          },
+        ),
 
-      // child: Scaffold(
-      //   body:
-      //       _cameraController != null && _cameraController!.value.isInitialized
-      //       ? Stack(
-      //           children: [
-      //             CameraPreview(_cameraController!),
-      //             Positioned(
-      //               right: 36,
-      //               top: 36,
-      //               width: 150,
-      //               height: 250,
-      //               child: Transform.flip(
-      //                 flipX: true,
-      //                 child: Transform.rotate(
-      //                   angle: -pi / 2,
-      //                   child: Center(
-      //                     child: ValueListenableBuilder(
-      //                       valueListenable: livenessChecker.currentImageStream,
-      //                       builder: (context, value, child) {
-      //                         // printLog('bio');
-      //                         if (value != null) {
-      //                           return Image.memory(
-      //                             value,
-      //                             fit: .contain,
-      //                             gaplessPlayback: true,
-      //                             // ImgUtils.convertToBytes(
-      //                             //   ImgUtils.convertCameraImageToImage(value!),
-      //                             // ),
-      //                           );
-      //                         }
-      //                         return Container(color: Colors.amber);
-      //                       },
-      //                     ),
-      //                   ),
-      //                 ),
-      //               ),
-      //             ),
-      //           ],
-      //         )
-      //       : Center(child: CircularProgressIndicator()),
-      // ),
+        // child: Scaffold(
+        //   body:
+        //       _cameraController != null && _cameraController!.value.isInitialized
+        //       ? Stack(
+        //           children: [
+        //             CameraPreview(_cameraController!),
+        //             Positioned(
+        //               right: 36,
+        //               top: 36,
+        //               width: 150,
+        //               height: 250,
+        //               child: Transform.flip(
+        //                 flipX: true,
+        //                 child: Transform.rotate(
+        //                   angle: -pi / 2,
+        //                   child: Center(
+        //                     child: ValueListenableBuilder(
+        //                       valueListenable: livenessChecker.currentImageStream,
+        //                       builder: (context, value, child) {
+        //                         // printLog('bio');
+        //                         if (value != null) {
+        //                           return Image.memory(
+        //                             value,
+        //                             fit: .contain,
+        //                             gaplessPlayback: true,
+        //                             // ImgUtils.convertToBytes(
+        //                             //   ImgUtils.convertCameraImageToImage(value!),
+        //                             // ),
+        //                           );
+        //                         }
+        //                         return Container(color: Colors.amber);
+        //                       },
+        //                     ),
+        //                   ),
+        //                 ),
+        //               ),
+        //             ),
+        //           ],
+        //         )
+        //       : Center(child: CircularProgressIndicator()),
+        // ),
+      ),
     );
   }
 }
@@ -282,7 +311,10 @@ class _DefaultLoadingView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: CircularProgressIndicator());
+    return Container(
+      color: Colors.amber,
+      child: Center(child: CircularProgressIndicator()),
+    );
   }
 }
 
